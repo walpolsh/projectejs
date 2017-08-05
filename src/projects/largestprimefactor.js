d9c8664fd56fa1b8cc27da32ac5a82c7644191da
Paul Walsh <paulwalsh@Pauls-MacBook-Pro.local>
2017-08-05 10:27:26 -0400

    largestpalindromeproduct

--- src/projects/largestprimefactor.js
+++ src/projects/largestprimefactor.js
@@ -1,20 +1,29 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 //
-function largestPrimeFactor(n){
-  var i=2;
+function largestPrimeFactor(n) {
+  var i = 2;
+
   // i is 2, a prime number
-  while ( i<=n) {
-  //while 2 is smaller or equal to our number run this test
-    if ( n%i == 0) {
-    //if the left over from dividing our number / 2 equals 0.
-      n/=i;
+
+  while (i <= n) {
+
+    //while 2 is smaller or equal to our number run this test
+
+    if (n % i == 0) {
+
+      //if the left over from dividing our number / 2 equals 0.
+
+      n /= i;
+
       // divide our number by 2 /= Division assignment
 
     } else {
-        i++;
-          // or else start incrementing i one (until its less than or equal to our number)
+      i++;
+
+      // or else start incrementing i one (until its less than or equal to our number)
     }
   };
+
   console.log(i);
 };
 
